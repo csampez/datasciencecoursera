66c989b725a41c3df5551b2f8e811e4d8be2b532
csampez <csampez@gmail.com>
2014-10-12 00:40:11 -0500

    ident fixed

--- ExData/plot1.R
+++ ExData/plot1.R
@@ -10,11 +10,13 @@ days<-strptime(c("01/02/2007 00:00:00","03/02/2007 00:00:00"), format="%d/%m/%Y 
 progdata<-data[which((days[1]<data$datetime_s)&(data$datetime_s<days[2])),]
 
 #plot params
-hist(progdata$Global_active_power, freq=TRUE, 
-									xlab="Global Active Power (kilowatts)", 
-									ylab="Frequency", 
-									main="Global Active Power" ,
-								 	col="Red")
+hist(progdata$Global_active_power, 
+		freq=TRUE, 
+		xlab="Global Active Power (kilowatts)", 
+		ylab="Frequency", 
+		main="Global Active Power" ,
+	 	col="Red")
+
 #saving to file
 dev.copy(png, file="./plot1.png", width=480, height=480)        
 dev.off()
