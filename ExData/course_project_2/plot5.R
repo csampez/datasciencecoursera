4902e8ee281c8cb65e267631c69e0627af42cb1d
csampez <csampez@gmail.com>
2014-10-26 15:16:30 -0600

    comment code

--- ExData/course_project_2/plot5.R
+++ ExData/course_project_2/plot5.R
@@ -1,13 +1,17 @@
+#library request
 library(plyr)
 library(dplyr)
 
-# fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-# download.file(fileUrl, destfile = "exdata_cp2.zip", method = "curl")
-# unzip("./exdata_cp2.zip")
+#data_download
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(fileUrl, destfile = "exdata_cp2.zip", method = "curl")
+unzip("./exdata_cp2.zip")
 
+#reading data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+#transformation
 motor_df<-NEI[NEI$SCC %in% SCC$SCC[grep("Vehicle",SCC$Short.Name)],]
 
 motor_sum_df<-data.frame(motor_df %>% 
@@ -15,6 +19,7 @@ motor_sum_df<-data.frame(motor_df %>%
 	 group_by(year,Pollutant) %>% 
 	 summarise( tot_emi = sum (Emissions)))
 
+#calling base plot system
 plot(motor_sum_df$year,motor_sum_df$tot_emi, type="b", main="Total Motor PM25-PRI Emissions in BC", xlab="Year", ylab="Total PM25-PRI emissions")
 
 #saving to file
